c886e1fd96515e59e3d903dd1c20b5249bf7672b
MishaStryzhka <misha.strizhcko@gmail.com>
2023-05-09 04:47:33 +0200

    update task-2

--- App.js
+++ App.js
@@ -1,31 +1,24 @@
 import React, { useState } from "react";
-import { useFonts } from "expo-font";
+import { isLoading, useFonts } from "expo-font";
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from "react";
 import {
   StyleSheet,
   View,
-  Text,
-  TextInput,
   TouchableWithoutFeedback,
   Keyboard,
   KeyboardAvoidingView,
   Platform,
-  Alert,
-  Button,
   ImageBackground,
-  TouchableOpacity,
 } from "react-native";
 import RegistrationScreen from "./Screens/RegistrationScreen/RegistrationScreen";
+import LoginScreen from "./Screens/LoginScreen/LoginScreen";
 
 export default function App() {
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
+  const [isRegistered, setIsRegistered] = useState(false);
 
   const [loadFonts] = useFonts({
-    // "Roboto-Regulat": require("./assets/fonts/Roboto/Roboto-Regular.ttf"),
-    "Roboto-Bold": require("./assets/fonts/Roboto/Roboto-Bold.ttf"),
-    "BebasNeue-Regular": require("./assets/fonts/Roboto/BebasNeue-Regular.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto/Roboto-Medium.ttf"),
     "Lobster-Regular": require("./assets/fonts/Lobster/Lobster-Regular.ttf"),
     "BrunoAceSC-Regular": require("./assets/fonts/Bruno/BrunoAceSC-Regular.ttf")
   });
@@ -36,110 +29,34 @@ export default function App() {
     }
   }, [])
 
-
   if (!loadFonts) {
     return undefined
   }
 
-  const nameHandler = (text) => setName(text);
-  const passwordHandler = (text) => setPassword(text);
-
-  const onLogin = () => {
-    Alert.alert("Credentials", `${name} + ${password}`);
+  const toggleRegistered = () => {
+    setIsRegistered(!isRegistered);
   };
+
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.container}>
-        <ImageBackground source={require('./assets/PhotoBG.png')} resizeMode="cover" style={styles.image}>
-          <RegistrationScreen>
-
-          </RegistrationScreen>
-
-
-          {/* <Text style={styles.textTitle}> MyTransfer</Text>
-          <View style={styles.section}>
-            <KeyboardAvoidingView
-              behavior={Platform.OS == "ios" ? "padding" : "height"}
-              style={styles.sectionLogin}
-            >
-              <TextInput
-                value={name}
-                onChangeText={nameHandler}
-                placeholder="Username"
-                style={styles.input}
-              />
-              <TextInput
-                value={password}
-                onChangeText={passwordHandler}
-                placeholder="Password"
-                secureTextEntry={true}
-                style={styles.input}
-              />
-              <TouchableOpacity style={styles.btnLogIn} onPress={onLogin}>
-                <Text>Přihlásit se</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.button} onPress={onLogin}>
-                <Text style={styles.text1}>Zaregistrovat se</Text>
-              </TouchableOpacity>
-            </KeyboardAvoidingView>
-          </View> */}
-        </ImageBackground>
+      <View>
+        <KeyboardAvoidingView // визначаємо ОС та налаштовуємо поведінку клавіатури
+          behavior={Platform.OS == "ios" ? "padding" : "height"}
+        >
+          <ImageBackground source={require('./assets/PhotoBG.png')} resizeMode="cover" style={styles.image}>
+            {!isRegistered && <RegistrationScreen toggleRegistered={toggleRegistered} />}
+            {isRegistered && <LoginScreen toggleRegistered={toggleRegistered} />}
+          </ImageBackground>
+        </KeyboardAvoidingView>
       </View>
     </TouchableWithoutFeedback>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    backgroundColor: "#ecf0f1",
-  },
-  section: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  sectionLogin: {
-    alignItems: "center",
-  },
   image: {
     height: "100%",
     width: "100%",
-  },
-  input: {
-    width: 200,
-    height: 44,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: "black",
-    marginBottom: 10,
-  },
-  btnLogIn: {
-    justifyContent: "center",
-    alignItems: "center",
-    width: 150,
-    height: 30,
-    borderWidth: 1,
-    borderColor: "black",
-    backgroundColor: "#ff8000",
-    color: "red",
-    marginBottom: 10,
-    marginTop: 20,
-  },
-  textTitle: {
-    position: "absolute",
-    top: 10,
-    left: 10,
-    fontSize: 35,
-    marginBottom: 20,
-    fontFamily: "BrunoAceSC-Regular",
-    color: "#ff8000",
-  },
-  text1: {
-    fontSize: 18,
-    fontFamily: "Roboto-Bold",
-    color: "#ff8000",
   },
 });
--- Screens/RegistrationScreen/RegistrationScreen.jsx
+++ Screens/RegistrationScreen/RegistrationScreen.jsx
@@ -1,9 +1,81 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useState } from "react";
+import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
-const RegistrationScreen = () => {
+const RegistrationScreen = ({ toggleRegistered }) => {
+    const [name, setName] = useState({
+        is: true,
+        value: "",
+    });
+    const [email, setEmail] = useState({
+        is: true,
+        value: "",
+    });
+    const [password, setPassword] = useState({
+        is: true,
+        value: "",
+    });
+
+
+
     const addImages = () => {
         Alert.alert("Add imades");
     };
+
+    const handlerShow = () => {
+        Alert.alert("Показать");
+    };
+
+    const handlerRegister = () => {
+        if (!name.is) { Alert.alert("Введите логин!"); }
+        else if (!email.is) { Alert.alert("Введите адрес электронной почты") }
+        else if (!password.is) { Alert.alert("Введите пароль") }
+        else {
+            console.log("name: ", name.value, "   email: ", email.value, "  password; ", password.value)
+            toggleRegistered()
+            Alert.alert(`Поздравляем ${name.value}! Вы зарегистрированы.`);
+        }
+    };
+
+    const nameHandler = (text) => {
+        if (text === "") {
+            setName({
+                is: false,
+                value: "",
+            })
+        } else {
+            setName({
+                is: true,
+                value: text,
+            })
+        }
+    };
+    const emailHandler = (text) => {
+        if (text === "") {
+            setEmail({
+                is: false,
+                value: "",
+            })
+        } else {
+            setEmail({
+                is: true,
+                value: text,
+            })
+        }
+    };
+    const passwordHandler = (text) => {
+        if (text === "") {
+            setPassword({
+                is: false,
+                value: "",
+            })
+        } else {
+            setPassword({
+                is: true,
+                value: text,
+            })
+        }
+    };
+
     return (
         <View style={styles.registrationContainer}>
             <View style={styles.imagesContainer}>
@@ -12,22 +84,63 @@ const RegistrationScreen = () => {
                     <View style={styles.icon2} />
                 </TouchableOpacity>
             </View>
-            <Text>Регистрация</Text>
+
+            <Text style={styles.titel}>Регистрация</Text>
+            <TextInput
+                value={name.value}
+                onChangeText={nameHandler}
+                placeholder="Логин"
+                style={name.is ? styles.input : styles.inputEror}
+                keyboardType="default"
+            />
+            <TextInput
+                value={email.value}
+                onChangeText={emailHandler}
+                placeholder="Адрес электронной почты"
+                style={email.is ? styles.input : styles.inputEror}
+                keyboardType="email-address"
+            />
+            <View>
+                <TextInput
+                    value={password.value}
+                    onChangeText={passwordHandler}
+                    placeholder="Пароль"
+                    style={password.is ? styles.input : styles.inputEror}
+                    keyboardType="default"
+                />
+                <TouchableOpacity style={styles.btnShow} onPress={handlerShow}>
+                    <Text>Показать</Text>
+                </TouchableOpacity>
+            </View>
+
+            <TouchableOpacity style={styles.btnRegister} onPress={handlerRegister}>
+                <Text style={styles.btnRegisterText}>Зарегистрироваться</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.btnSignIn} onPress={() => { toggleRegistered() }}>
+                <Text style={styles.btnSignInText}>Уже есть аккаунт? Войти</Text>
+            </TouchableOpacity>
+
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    // Registration container
+
     registrationContainer: {
         position: "absolute",
         bottom: 0,
-        height: 550,
+        height: 500,
         width: "100%",
         borderTopLeftRadius: 25,
         borderTopRightRadius: 25,
-        // justifyContent: "center",
         backgroundColor: "#FFFFFF",
     },
+
+    // Container for image 
+
     imagesContainer: {
         position: "absolute",
         alignSelf: 'center',
@@ -37,16 +150,15 @@ const styles = StyleSheet.create({
         backgroundColor: "#F6F6F6",
         borderRadius: 16,
     },
-    btnAddsImages: {
 
+    // Button "add images"
+
+    btnAddsImages: {
         position: "absolute",
         right: -12.5,
         bottom: 26.5,
         width: 25,
         height: 25,
-        // left: 235
-        // top: 284px;
-        // backgroundColor: "#FFFFFF",
         borderWidth: 1,
         borderColor: "#FF6C00",
         borderRadius: 25,
@@ -64,7 +176,98 @@ const styles = StyleSheet.create({
         width: 13,
         height: 1,
         backgroundColor: "#FF6C00",
-    }
+    },
+
+    // Titel "Регистрация"
+
+    titel: {
+        fontFamily: "Roboto-Medium",
+        fontWeight: 500,
+        fontSize: 30,
+        lineHeight: 35,
+        textAlign: "center",
+        color: "#212121",
+
+        marginTop: 92,
+        marginBottom: 33,
+    },
+
+    // Input 
+    input: {
+        height: 50,
+        padding: 16,
+        borderWidth: 1,
+        backgroundColor: "#F6F6F6",
+        borderColor: "#E8E8E8",
+        marginBottom: 16,
+        marginLeft: 16,
+        marginRight: 16,
+        borderRadius: 8,
+    },
+
+    inputEror: {
+        height: 50,
+        padding: 16,
+        borderWidth: 1,
+        backgroundColor: "#F6F6F6",
+        borderColor: "red",
+        marginBottom: 16,
+        marginLeft: 16,
+        marginRight: 16,
+        borderRadius: 8,
+    },
+
+    // Button Show
+
+    btnShow: {
+        position: "absolute",
+        top: 14,
+        right: 32,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+
+    // Button Registr
+
+    btnRegister: {
+        height: 50,
+        padding: 16,
+        marginBottom: 16,
+        marginLeft: 16,
+        marginRight: 16,
+        borderRadius: 100,
+        alignItems: "center",
+
+        backgroundColor: "#FF6C00",
+    },
+
+    btnRegisterText: {
+        fontFamily: "Roboto-Medium",
+        fontStyle: "normal",
+        fontWeight: 400,
+        fontSize: 16,
+        lineHeight: 19,
+
+        color: "#FFFFFF",
+    },
+
+    // Button SignIn
+
+    btnSignIn: {
+        alignItems: "center",
+    },
+
+    btnSignInText: {
+        fontFamily: "Roboto-Medium",
+        fontStyle: "normal",
+        fontWeight: 400,
+        fontSize: 16,
+        lineHeight: 19,
+        color: "#1B4371",
+    },
+
+
+
 });
 
 export default RegistrationScreen;
